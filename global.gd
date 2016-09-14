ead7fdbd32276d9daf811e09811a9e6693c44def
Maveyyl <martialveyry@hotmail.fr>
2016-09-14 13:05:19 +0200

    Added a global singleton file

---
+++ global.gd
@@ -0,0 +1,27 @@
+extends Node
+
+
+
+const faces = {
+	front = 0,
+	back = 1,
+	right = 2,
+	left = 3,
+	top = 4,
+	bottom = 5,
+	count = 6
+}
+
+
+
+const voxel_types = {
+	empty = 0,
+	blue = 1,
+	green = 2,
+	red = 3,
+	count = 4
+}
+
+
+func _ready():
+	pass
