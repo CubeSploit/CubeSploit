87ec8f5a5931a81fa7d1baf07f0b92ba26e38dc1
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2017-02-03 18:32:28 +0200

    Add psuedo-cubaical gravity and make the player rotate according to it

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,9 @@
-extends KinematicBody
+extends RigidBody
 
-var velocity = Vector3(0,0,0)
 var speed = 50
 var angular_speed = 0.05
+var rerotation_speed = 10
+var observed_force = Vector3()
 
 onready var last_mouse_pos = get_viewport().get_mouse_pos()
 onready var camera = get_node("camera")
@@ -10,8 +11,7 @@ onready var camera = get_node("camera")
 func _ready():
 	Input.call_deferred("set_mouse_mode", Input.MOUSE_MODE_CAPTURED )
 	set_fixed_process(true)
-	
-	
+
 func _fixed_process(delta):
 	# Getting delta movement of mouse
 	var current_mouse_pos = get_viewport().get_mouse_pos()
@@ -22,38 +22,55 @@ func _fixed_process(delta):
 	last_mouse_pos = current_mouse_pos
 #	print(delta_mouse_pos)
 	
-	rotate_y( delta_mouse_pos.x * angular_speed * delta )
+	var current_matrix = get_global_transform().basis
+	var current_up = current_matrix.xform(Vector3(0,1,0))
+	var target_up = -observed_force.normalized()
+	if target_up.cross(current_up).length_squared() > 0.001:
+		var new_current_matrix = fixed_rotate_matrix(current_matrix, target_up.cross(current_up), target_up.angle_to(current_up) * delta * rerotation_speed)
+		var new_current_up = current_up.rotated(target_up.cross(current_up), target_up.angle_to(current_up) * delta * rerotation_speed)
+#		assert(target_up.distance_to(new_current_up) < 0.1)
+#		assert(target_up.distance_to(new_current_matrix.xform(Vector3(0,1,0))) < 0.1)
+		current_matrix = new_current_matrix
+	
+	current_matrix = fixed_rotate_matrix(current_matrix, target_up, delta_mouse_pos.x * angular_speed * delta)
+	
+	set_global_transform(Transform(current_matrix, get_global_transform().origin))
+	
 	camera.rotate_x( delta_mouse_pos.y * angular_speed * delta )
 #	print(get_rotation())
 	
+	var velocity_change = Vector3()
+	
 	if( Input.is_action_pressed("move_forward") ):
-		velocity.z = - speed * delta
+		velocity_change.z = - speed * delta
 	elif( Input.is_action_pressed("move_backward") ):
-		velocity.z = speed * delta
-	else:
-		velocity.z = 0
+		velocity_change.z = speed * delta
 		
 	if( Input.is_action_pressed("move_left") ):
-		velocity.x = - speed * delta
+		velocity_change.x = - speed * delta
 	elif( Input.is_action_pressed("move_right") ):
-		velocity.x = speed * delta
-	else:
-		velocity.x = 0
+		velocity_change.x = speed * delta
 		
 	if( Input.is_action_pressed("jump") ):
-		velocity.y = speed * delta
+		velocity_change.y = speed * delta
 	elif( Input.is_action_pressed("crouch") ):
-		velocity.y = - speed * delta
-	else:
-		velocity.y = 0
+		velocity_change.y = - speed * delta
 
 	# We want the player to move forward and backward to/from where it is looking at
 	# Don't ask me why this does the work.
-	var to_move = get_transform().basis * camera.get_transform().basis * velocity
-	to_move = move(to_move)
-	if is_colliding():
-		to_move = move(get_collision_normal().slide(to_move))
+#	velocity_change = get_transform().basis * camera.get_transform().basis * velocity_change
+	velocity_change = get_transform().basis * velocity_change
+	
+	set_linear_velocity(get_linear_velocity() + velocity_change)
+	observed_force = Vector3()
 
-	
-	
-	
+func apply_impulse(pos, impulse):
+	.apply_impulse(pos, impulse)
+	observed_force += impulse
+
+func fixed_rotate_matrix(matrix, axis, angle):
+	return Matrix3(
+		matrix.x.rotated(axis, angle),
+		matrix.y.rotated(axis, angle),
+		matrix.z.rotated(axis, angle)
+	)
--- world/body/body.gd
+++ world/body/body.gd
@@ -7,7 +7,33 @@ export(Material) var voxel_material_text
 
 var chunk_size = 8
 var chunks = [] # 3D array containing the chunks of the body
-
+var center = Vector3()
+var mass = 0
 var octree_root
+
+onready var gravity_area = get_node("gravity_area")
+
+func _ready():
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	var position = get_global_transform().origin + center
+	for attracted_body in gravity_area.get_overlapping_bodies():
+		if attracted_body extends RigidBody:
+#			attracted_body = RigidBody.new()
+			var other_position = attracted_body.get_global_transform().origin
+			var difference = position - other_position
+			var difference_abs = Vector3(abs(difference.x), abs(difference.y), abs(difference.z))
+			var max_axis = difference_abs.max_axis()
+			var force = Vector3(0,0,0)
+			force[max_axis] = 20 * sign(difference[max_axis])
+			attracted_body.apply_impulse(other_position, force * delta)
+
 func generate_random_octree( body_size ):
 	octree_root = OctreeNode.new(body_size, self)
+	center = Vector3(1,1,1) * body_size / 2
+	mass = body_size * body_size * body_size
+	
+	var shape = SphereShape.new()
+	shape.set_radius(body_size * 10)
+	gravity_area.add_shape(shape)
