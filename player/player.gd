24434c8b16ccd2efab9cb71965c79ec2ad49d192
Maveyyl <martialveyry@hotmail.fr>
2017-02-04 11:02:59 +0100

    Minor player movements corrections, added toggle free fly/normal mode, added jump

--- player/player.gd
+++ player/player.gd
@@ -1,10 +1,14 @@
 extends RigidBody
 
 var speed = 50
+var jump_force = 1000
 var angular_speed = 0.05
-var rerotation_speed = 10
+var rerotation_speed = 15
 # the force environment applies on the player, usually it is gravity
-var observed_force = Vector3()
+var observed_gravity = Vector3()
+
+var is_grounded = false
+var free_fly = false
 
 onready var last_mouse_pos = get_viewport().get_mouse_pos()
 onready var camera = get_node("camera")
@@ -31,34 +35,13 @@ func _fixed_process(delta):
 	camera.rotate_x( delta_mouse_pos.y * angular_speed * delta )
 	
 	# here we handle rotation of players according to gravity
-	
-	# get the basis of the player's transform
-	var current_matrix = get_transform().basis
-	# get the up vector transformed by the player's perspective
-	var current_up = current_matrix.xform(Vector3(0,1,0))
-	# get what should be the up vector of the player by getting the inverse of the applied gravity on the player
-	var target_up = -observed_force.normalized()
-
-	var rotation_axis = target_up.cross(current_up)
-	# the cross product outputs a vector representing the axis of rotation between the two product
-	# and which amplitude is the area of the triangle formed by the two vectors
-	# if target_up and current_up aren't collinear
-	if( rotation_axis.length() != 0 ):
-		# get the angle between target_up and current_up
-		var angle_to_target_up = target_up.angle_to(current_up)
-		# compute the value of the rotation for this step
-		var rotation_value = sign(angle_to_target_up) * delta * rerotation_speed
-		# if by applying that rotation we get beyond the target angle, reset it to target angle
-		if( rotation_value > angle_to_target_up ):
-			rotation_value = angle_to_target_up
-			
-		# get the basis matrix rotated toward the target_up vector by a step
-		var new_current_matrix = fixed_rotate_matrix(current_matrix, rotation_axis, rotation_value)
-		# also rotate the velocity of the player, feels more natural
-		set_linear_velocity(get_linear_velocity().rotated(rotation_axis, rotation_value))
-		
-		# set the new computed matrix as the used matrix
-		set_transform(Transform(new_current_matrix, get_global_transform().origin))
+	# if there's gravity applied to the player
+	if( observed_gravity.length() != 0 ):
+		# get what should be the up vector of the player by getting the inverse of the applied gravity on the player
+		set_player_up_vector(-observed_gravity.normalized(), delta )
+	else:
+		# else set the up vector by default
+		set_player_up_vector(Vector3(0,1,0), delta)
 	
 	
 	var velocity_change = Vector3()
@@ -74,14 +57,28 @@ func _fixed_process(delta):
 		velocity_change.x = speed * delta
 		
 	if( Input.is_action_pressed("jump") ):
-		velocity_change.y = speed * delta
+		if( observed_gravity.length() == 0 || free_fly ):
+			velocity_change.y = speed * delta
+		elif( is_grounded ):
+			velocity_change.y = jump_force * delta
+			is_grounded = false
 	elif( Input.is_action_pressed("crouch") ):
-		velocity_change.y = - speed * delta
+		if( observed_gravity.length() == 0 || free_fly ):
+			velocity_change.y = - speed * delta
+			
+	if( Input.is_action_just_pressed("free_fly") ):
+		free_fly = !free_fly
 
-	if( observed_force.length() == 0 ):
+	if( observed_gravity.length() == 0 || free_fly ):
+		# tricky part
+		
 		# We want the player to move forward and backward to/from where it is looking at
 		# So the velicoty change is applied on the camera basis, and then on the player's basis
-		velocity_change = get_transform().basis.xform(camera.get_transform().basis.xform(velocity_change))
+		
+		# we get the x and z movements according to the camera's perspective, and then we add the raw y movements
+		var velocity_change_camera_perspective = camera.get_transform().basis.xform(velocity_change * Vector3(1,0,1)) + velocity_change * Vector3(0,1,0)
+		# we change all movements into the player's perspective, and we get the movement for the player toward the world
+		velocity_change = get_transform().basis.xform(velocity_change_camera_perspective)
 	else:
 		# When under gravity, the player isn't moving forward and backward where he is looking at
 		# but he's moving forward and backward according to its basis
@@ -90,16 +87,55 @@ func _fixed_process(delta):
 	# apply the velocity changes
 	set_linear_velocity(get_linear_velocity() + velocity_change)
 	
-	# reset the observed forces
-	observed_force = Vector3()
+	# reset the observed gravity
+	observed_gravity = Vector3()
 
-func apply_impulse(pos, impulse):
-	.apply_impulse(pos, impulse)
-	observed_force += impulse
+func apply_gravity(pos, impulse):
+#	apply_impulse(pos, impulse)
+	observed_gravity += impulse
+
+
+func set_player_up_vector(target_up, delta):
+	# get the basis of the player's transform
+	var current_matrix = get_transform().basis
+	# get the up vector transformed by the player's perspective
+	var current_up = current_matrix.xform(Vector3(0,1,0))
+
+	var rotation_axis = target_up.cross(current_up)
+	# the cross product outputs a vector representing the axis of rotation between the two product
+	# and which amplitude is the area of the triangle formed by the two vectors
+	# if target_up and current_up aren't collinear
+	if( rotation_axis.length() != 0 ):
+		# get the angle between target_up and current_up
+		var angle_to_target_up = target_up.angle_to(current_up)
+		# compute the value of the rotation for this step
+		var rotation_value = sign(angle_to_target_up) * delta * rerotation_speed
+		# if by applying that rotation we get beyond the target angle, reset it to target angle
+		if( rotation_value > angle_to_target_up ):
+			rotation_value = angle_to_target_up
+			
+		# get the basis matrix rotated toward the target_up vector by a step, normalized axis is important
+		var new_current_matrix = fixed_rotate_matrix(current_matrix, rotation_axis.normalized(), rotation_value)
+		# also rotate the velocity of the player, feels more natural
+		set_linear_velocity(get_linear_velocity().rotated(rotation_axis.normalized(), rotation_value))
+		
+		# set the new computed matrix as the used matrix
+		set_transform(Transform(new_current_matrix, get_global_transform().origin))
 
 func fixed_rotate_matrix(matrix, axis, angle):
+#	return matrix.rotated(axis,angle) # why this doesn't work at all???
 	return Matrix3(
 		matrix.x.rotated(axis, angle),
 		matrix.y.rotated(axis, angle),
 		matrix.z.rotated(axis, angle)
 	)
+
+
+func _on_player_body_enter_shape( body_id, body, body_shape, local_shape ):
+#	print(body_id, " ", body, " ", body_shape, " ", local_shape)
+	if( local_shape == 1 ):
+		is_grounded = true
+
+func _on_player_body_exit_shape( body_id, body, body_shape, local_shape ):
+	if( local_shape == 1 ):
+		is_grounded = false
--- world/body/body.gd
+++ world/body/body.gd
@@ -17,17 +17,22 @@ func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	var position = get_global_transform().origin + center
+	# get the relative position of the center body
+	var position = get_transform().origin + center
+	# for each rigid bodies in the gravity area of the body
 	for attracted_body in gravity_area.get_overlapping_bodies():
 		if attracted_body extends RigidBody:
-#			attracted_body = RigidBody.new()
-			var other_position = attracted_body.get_global_transform().origin
+			# get the position of the rigid body
+			var other_position = attracted_body.get_transform().origin
+			# get the distance vector between both
 			var difference = position - other_position
+			# get the highest coordinate of the distance vector
 			var difference_abs = Vector3(abs(difference.x), abs(difference.y), abs(difference.z))
 			var max_axis = difference_abs.max_axis()
+			# apply gravity to the rigid body according to its position to the body
 			var force = Vector3(0,0,0)
 			force[max_axis] = 20 * sign(difference[max_axis])
-			attracted_body.apply_impulse(other_position, force * delta)
+			attracted_body.apply_gravity(other_position, force * delta)
 
 func generate_random_octree( body_size ):
 	octree_root = OctreeNode.new(body_size, self)
